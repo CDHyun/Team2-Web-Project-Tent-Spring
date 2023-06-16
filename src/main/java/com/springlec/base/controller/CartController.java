806ee554e3cdc4e94bee723ff9b5833e9d55f8d9
leenakyung <skruddl1003@gmail.com>
2023-06-16 19:23:28 +0900

    Cart 초기

--- src/main/java/com/springlec/base/controller/CartController.java
+++ src/main/java/com/springlec/base/controller/CartController.java
@@ -1,11 +1,30 @@
 package com.springlec.base.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springlec.base.model.CartDto;
+import com.springlec.base.service.CartService;
 
 @Controller
 public class CartController {
-
-	// 카트 데이터 입력 
 	
+	@Autowired
+	CartService service;
+	
+	
+	
+
+	// 카트 리스트 가져오기
+	@RequestMapping("/adminCart")
+	public String list(Model model) throws Exception{
+		List<CartDto> listDao = service.listDao();
+		model.addAttribute("list", listDao);
+		return "list";
+	}
 	
 }// End
