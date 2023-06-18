ab9561e867ef4ee0c5e88a704311d625f2f626fd
Donghyun <pdchoirian@gmail.com>
2023-06-18 17:10:32 +0900

    공지사항 조회수 증가 구현

--- src/main/java/com/springlec/base/controller/NoticeController.java
+++ src/main/java/com/springlec/base/controller/NoticeController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.springlec.base.model.Notice;
 import com.springlec.base.service.NoticeService;
@@ -23,12 +24,21 @@ public class NoticeController {
 	@Autowired
 	NoticeService noticeService;
 	
+	// 공지사항 전체 게시글 불러오기
 	@RequestMapping("notice_list")
 	public String noticeList(HttpServletRequest request, Model model) throws Exception {
 		
 		List<Notice> noticeList = noticeService.noticeList();
 		model.addAttribute("noticeList", noticeList);
 		return "notice/notice";
+	}
+	
+	// 공지사항 조회수 증가
+	@RequestMapping("increaseNoticeViewCount")
+	@ResponseBody
+	public int increaseNoticeViewCount(HttpServletRequest request) throws Exception {
+		int result = noticeService.increaseNoticeViewCount(Integer.parseInt(request.getParameter("nNo")));
+		return result;
 	}
 	
 	
